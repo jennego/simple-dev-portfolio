23ed5f0a85605eaf9076edc1f4ecfeca4cd1fd16
Jennifer Chow <jennego@gmail.com>
2021-12-08 00:30:19 -0800

    begin to play with react-plx

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,28 +4,93 @@ import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Plx from "react-plx"
+import background from "../images/background.jpg"
+import ray from "../images/ray.png"
+import grass from "../images/grass.png"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
-  </Layout>
-)
+const IndexPage = () => {
+  const parallaxData = [
+    {
+      start: 0,
+      end: 500,
+      properties: [
+        {
+          startValue: 1,
+          endValue: 1.2,
+          property: "scale",
+        },
+      ],
+    },
+    {
+      start: 0,
+      end: 500,
+      properties: [
+        {
+          startValue: 0,
+          endValue: -50,
+          property: "translateY",
+        },
+      ],
+    },
+  ]
+
+  const parallaxDataHorse = [
+    {
+      start: 0,
+      end: 500,
+      properties: [
+        {
+          startValue: 1.06,
+          endValue: 1,
+          property: "scale",
+        },
+      ],
+    },
+  ]
+
+  const parallaxDataGrass = [
+    {
+      start: 0,
+      end: 500,
+      properties: [
+        {
+          startValue: 1,
+          endValue: 1.5,
+          property: "scale",
+        },
+      ],
+    },
+  ]
+  return (
+    <Layout>
+      <h1>Hello World</h1>
+      <div
+        style={{
+          width: "1000px",
+          height: "500px",
+          overflow: "hidden",
+          border: "2px black solid",
+        }}
+      >
+        <Plx
+          className="MyAwesomeParallax"
+          style={{ position: "absolute" }}
+          parallaxData={parallaxData}
+        >
+          <img src={background} width="1000px" />
+        </Plx>
+
+        <Plx style={{ position: "absolute" }} parallaxData={parallaxDataHorse}>
+          <img src={ray} width="1000px" />
+        </Plx>
+
+        <Plx style={{ position: "absolute" }} parallaxData={parallaxDataGrass}>
+          <img src={grass} width="1000px" />
+        </Plx>
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
